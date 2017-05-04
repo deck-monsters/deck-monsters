cdb443b99aeba3714464d59333cfe8bf84e51841
David Brainer-Banker <dbrainer-banker@salesforce.com>
2017-05-04 08:25:31 -0600

    Update curse to use the new dice

--- cards/curse.js
+++ cards/curse.js
@@ -12,28 +12,44 @@ class CurseCard extends BaseCard {
 		super(Object.assign(defaultOptions, options));
 	}
 
-	static effect (player, target, game) { // eslint-disable-line no-unused-vars
-		let weakenRoll = roll(this.healthDice);
+	get weakenDice () {
+		return this.options.weakenDice;
+	}
+
+	get stats () {
+		return `Curse: ${this.weakenDice}`;
+	}
+
+	effect (player, target, game) { // eslint-disable-line no-unused-vars
+		const weakenRoll = roll(this.weakenDice);
+		let weakenResult = 0 - weakenRoll.result;
 		let strokeOfLuck = false;
 		let curseOfLoki = false;
 
 		// Stroke of Luck
 		if (isProbable({ probability: 1 })) {
-			weakenRoll = 6;
+			weakenResult = -6;
 			strokeOfLuck = true;
 		} else if (isProbable({ probability: 10 })) {
-			weakenRoll = 0;
+			weakenResult = 0;
 			curseOfLoki = true;
 		}
 
-		this.emit('rolled', { weakenRoll, player, strokeOfLuck, curseOfLoki });
+		this.emit('rolled', {
+			curseOfLoki,
+			player,
+			strokeOfLuck,
+			target,
+			weakenResult,
+			weakenRoll
+		});
 
-		target.condition('ac', weakenRoll);
+		target.condition('ac', weakenResult);
 	}
 }
 
 CurseCard.cardType = 'Weaken';
 CurseCard.probability = 10;
-CurseCard.description = "Sweep the leg... You have a problem with that? No mercy.";
+CurseCard.description = 'Sweep the leg... You have a problem with that? No mercy.';
 
 module.exports = CurseCard;
