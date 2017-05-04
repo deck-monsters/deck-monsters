d55c45ecf94413e795e079aa2b733cda7e356003
David Brainer-Banker <dbrainer-banker@salesforce.com>
2017-05-04 00:13:30 -0600

    Add the monster spawn function

--- monsters/index.js
+++ monsters/index.js
@@ -8,6 +8,41 @@ const all = [
 	WeepingAngel
 ];
 
+// Callback should be a function that takes a question and an optional array of
+// choices and returns an answer to the question (or a Promise that resolves to
+// an answer to the question)
+const spawn = (callback) => {
+	const monsterTypes = all.map(monster => monster.creatureType);
+	const options = {};
+	let Monster;
+
+	return Promise
+		.resolve()
+		.then(() => callback({
+			question: 'Which type of monster would you like to spawn?',
+			choices: monsterTypes
+		}))
+		.then((answer) => {
+			Monster = all.find(monster => monster.creatureType.toLowerCase() === answer.toLowerCase());
+
+			return callback({
+				question: `What would you like to name your new ${Monster.creatureType}?`
+			});
+		})
+		.then((answer) => {
+			options.name = answer;
+
+			return callback({
+				question: `What color should ${options.name} be?`
+			});
+		})
+		.then((answer) => {
+			options.color = answer;
+
+			return new Monster(options);
+		});
+};
+
 const hydrateMonster = (monsterObj) => {
 	const Monster = all.find(({ name }) => name === monsterObj.name);
 	return new Monster(monsterObj.options);
@@ -19,6 +54,7 @@ const hydrateMonsters = monstersJSON => JSON
 
 module.exports = {
 	all,
+	spawn,
 	hydrateMonster,
 	hydrateMonsters
 };
