e7dd0fcfe73db551050e913605cc49edf8e45b7b
David Brainer-Banker <dbrainer-banker@salesforce.com>
2017-05-04 15:15:50 -0600

    Add a battle loop

--- ring/index.js
+++ ring/index.js
@@ -63,12 +63,45 @@ class Ring {
 	}
 
 	fight () {
+		const game = this; // TO-DO: This is actually the ring, so we might want to think about it
 		const contestants = this.contestants;
 
 		this.emit('fight', {
 			contestants,
 			monsters: contestants.map(contestant => contestant.monster)
 		});
+
+		let fightContinues;
+		let currentContestant = 0;
+		let currentCard = 0;
+		let emptyHanded = false;
+
+		// TO-DO: Make this async so that we can add a little delay between each step (for more interesting results)
+		do {
+			const contestant = contestants[currentContestant];
+			const monster = contestant.monster;
+			const card = monster.cards[currentCard];
+
+			let nextContestant = currentContestant + 1;
+			if (nextContestant >= contestants.length) {
+				nextContestant = 0;
+			}
+
+			if (card) {
+				emptyHanded = false;
+				fightContinues = card.effect(monster, contestants[nextContestant].monster, game);
+			} else if (emptyHanded === false) {
+				emptyHanded = currentContestant;
+			}
+
+			if (emptyHanded === nextContestant) {
+				currentCard = 0;
+			} else {
+				currentCard += 1;
+			}
+
+			currentContestant = nextContestant;
+		} while (fightContinues === true);
 	}
 
 	emit (event, ...args) {
