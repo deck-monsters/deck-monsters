3fa2015d3202a0ae4d2435a915dcdfd4b2ced04c
David Brainer-Banker <dbrainer-banker@salesforce.com>
2017-05-04 01:14:27 -0600

    Add a spawnMonster command to players

--- players/base.js
+++ players/base.js
@@ -1,5 +1,6 @@
 const BaseCreature = require('../creatures/base');
 const { getInitialDeck } = require('../cards');
+const { spawn } = require('../monsters');
 
 const DEFAULT_MONSTER_SLOTS = 2;
 
@@ -44,6 +45,34 @@ class BasePlayer extends BaseCreature {
 
 	addCard (card) {
 		this.cards = [...this.cards, card];
+	}
+
+	addMonster (monster) {
+		this.monsters = [...this.monsters, monster];
+	}
+
+	spawnMonster (callback) {
+		const remainingSlots = Math.max(this.monsterSlots - this.monsters.length, 0);
+
+		if (remainingSlots > 0) {
+			return Promise
+				.resolve()
+				.then(() => callback({
+					announce: `You have ${remainingSlots} of ${this.monsterSlots} monsters left to train.`
+				}))
+				.then(() => spawn(callback))
+				.then((monster) => {
+					this.addMonster(monster);
+
+					return callback({
+						announce: `You're now the proud owner of ${monster.givenName}. Before you is ${monster.individualDescription}'`
+					});
+				});
+		}
+
+		return Promise.reject(() => callback({
+			announce: "You're all out space for new monsters!"
+		}));
 	}
 }
 
