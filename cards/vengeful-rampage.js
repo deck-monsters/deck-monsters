24b42503b2752729946a4028612bc406c6e5223c
David Brainer-Banker <dbrainer-banker@salesforce.com>
2017-12-01 21:30:16 -0700

    Add some flavors

--- cards/vengeful-rampage.js
+++ cards/vengeful-rampage.js
@@ -18,4 +18,14 @@ VenegefulRampageCard.description = 'Your wounds only make you stronger.';
 VenegefulRampageCard.level = 3;
 VenegefulRampageCard.permittedClasses = [BARBARIAN];
 
+VenegefulRampageCard.flavors = {
+	hits: [
+		['fights back against', 80],
+		['screams with rage against', 70],
+		['eviscerates', 70],
+		['curses the very existance of', 50],
+		['glows red with rage and annihilates', 5]
+	]
+};
+
 module.exports = VenegefulRampageCard;
