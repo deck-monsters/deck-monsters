ba831dd03d90117926896159e38951b431c56565
Christopher McCulloh <cmcculloh@salesforce.com>
2017-12-07 16:31:09 -0500

    (GH42) make a destroy card to test permadeath with and allow the use of it in battlefield

---
+++ cards/destroy.js
@@ -0,0 +1,59 @@
+/* eslint-disable max-len */
+
+const BaseCard = require('./base');
+
+class DestroyCard extends BaseCard {
+	// Set defaults for these values that can be overridden by the options passed in
+	constructor ({
+		damage,
+		icon = '☠️',
+		levelDamage
+	} = {}) {
+		super({ damage, icon, levelDamage });
+	}
+
+	get damage () {
+		return this.options.damage;
+	}
+
+	get levelDamage () {
+		return this.options.levelDamage;
+	}
+
+	get stats () {
+		return `Destroy: Annihilates your opponent`;
+	}
+
+	effect (player, target, ring, activeContestants) {
+		const damage = this.damage + (this.levelDamage * player.level);
+
+		return new Promise((resolve) => {
+			resolve(Promise.all(activeContestants.map(({ monster }) => {
+				if (monster !== player) {
+					return monster.hit(damage, player, this);
+				}
+
+				return Promise.resolve();
+			}))
+				.then(() => !target.dead));
+		});
+	}
+}
+
+DestroyCard.cardType = 'Destroy';
+DestroyCard.probability = 0;
+DestroyCard.description = 'A test card used to completely destroy your opponent.';
+DestroyCard.cost = 4;
+DestroyCard.level = 0;
+DestroyCard.defaults = {
+	damage: 9999999999999,
+	levelDamage: 1
+};
+
+DestroyCard.flavors = {
+	hits: [
+		['annihilates', 100]
+	]
+};
+
+module.exports = DestroyCard;
