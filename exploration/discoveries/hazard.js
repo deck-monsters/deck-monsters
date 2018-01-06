ed2d3293b7adee835c8c9a597e807876edabdc7c
Christopher McCulloh <cmcculloh@salesforce.com>
2018-01-06 10:13:43 -0500

    (GH12) makes hazards hurt player

--- exploration/discoveries/hazard.js
+++ exploration/discoveries/hazard.js
@@ -1,21 +1,38 @@
 /* eslint-disable max-len */
 
 const BaseCard = require('./base');
+const { roll } = require('../../helpers/chance');
 
 class HazardCard extends BaseCard {
 	// Set defaults for these values that can be overridden by the options passed in
 	constructor ({
+		damageDice,
 		icon = '⚠️'
 	} = {}) {
-		super({ icon });
+		super({ damageDice, icon });
+	}
+
+	get damageDice () {
+		return this.options.damageDice;
+	}
+
+	set damageDice (damageDice) {
+		this.setOptions({
+			damageDice
+		});
 	}
 
 	get stats () {
 		return this.flavor;
 	}
 
-	effect (player) { // eslint-disable-line no-unused-vars
-		player.dead = true;
+	effect (player) {
+		const assailant = {
+			icon: this.icon,
+			givenName: 'The World'
+		}
+		const damage = roll({ primaryDice: this.damageDice }).result;
+		player.hit(damage, assailant, this);
 
 		return player;
 	}
@@ -24,11 +41,14 @@ class HazardCard extends BaseCard {
 HazardCard.cardType = 'Hazard';
 HazardCard.probability = 10;
 HazardCard.description = 'It is dangerous out there...';
-
 HazardCard.flavors = {
 	hazard: [
 		['you fall and hit your head', 100]
 	]
 };
+HazardCard.defaults = {
+	damageDice: '1d4'
+};
+
 
 module.exports = HazardCard;
