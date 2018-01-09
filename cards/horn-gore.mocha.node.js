19f32f931dfdcc98c331f81faedeceafe1485312
Christopher McCulloh <cmcculloh@salesforce.com>
2018-01-09 11:16:54 -0500

    nerfs horn gore slightly

--- cards/horn-gore.mocha.node.js
+++ cards/horn-gore.mocha.node.js
@@ -52,6 +52,14 @@ describe('./cards/horn-gore.js', () => {
 		expect(hornGore.permittedClassesAndTypes).to.deep.equal([MINOTAUR]);
 	});
 
+	it('only uses half of their damage modifier when calculating damage for a horn', () => {
+		const hornGore = new HornGoreCard();
+		const player = new Minotaur({ name: 'player' });
+		const damageRoll = hornGore.getDamageRoll(player);
+
+		expect(damageRoll.modifier).to.deep.equal(1);
+	});
+
 	it('hits twice and immobilizes', () => {
 		const hornGore = new HornGoreCard();
 		const player = new Minotaur({ name: 'player' });
