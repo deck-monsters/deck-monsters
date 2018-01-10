2d44fd8afe54191c6b23372b42e4d23c3887b2fa
David Brainer-Banker <dbrainer-banker@salesforce.com>
2018-01-10 15:45:29 -0700

    Move card selection out of card equip

--- cards/index.js
+++ cards/index.js
@@ -1,5 +1,8 @@
 const shuffle = require('lodash.shuffle');
 
+const { getCardChoices, getFinalCardChoices } = require('../helpers/choices');
+const getArray = require('../helpers/get-array');
+
 const isProbable = require('../helpers/is-probable');
 const BasicShieldCard = require('./basic-shield');
 const BattleFocusCard = require('./battle-focus');
@@ -182,8 +185,65 @@ const hydrateDeck = (deckJSON = [], monster) => {
 	return deck;
 };
 
+const cardChoiceQuestion = ({ cardChoices }) => `Choose one or more of the following cards:
+
+${cardChoices}`;
+
+const cardChoiceResult = ({ selectedCards }) => {
+	if (selectedCards.length <= 0) {
+		return 'You selected no cards.';
+	} else if (selectedCards.length === 1) {
+		return `You selected a ${selectedCards[0].cardType.toLowerCase()} card.`;
+	}
+
+	return `You selected the following cards:
+
+${getFinalCardChoices(selectedCards)}`;
+};
+
+const chooseCards = ({
+	cards,
+	channel,
+	getQuestion = cardChoiceQuestion,
+	getResult = cardChoiceResult
+}) => {
+	const cardCatalog = getCardCounts(cards);
+	const cardChoices = getCardChoices(cardCatalog);
+
+	return Promise
+		.resolve()
+		.then(() => channel({
+			question: getQuestion({ cardChoices })
+		}))
+		.then((answer) => {
+			let selectedCardIndexes = getArray(answer);
+			if (!Array.isArray(selectedCardIndexes)) selectedCardIndexes = [selectedCardIndexes];
+
+			const remainingCards = [...cards];
+			const selectedCards = selectedCardIndexes.reduce((selection, index) => {
+				const cardType = Object.keys(cardCatalog)[index - 0];
+				const cardIndex = remainingCards.findIndex(potentialCard => potentialCard.cardType === cardType);
+
+				if (cardIndex >= 0) {
+					const selectedCard = remainingCards.splice(cardIndex, 1)[0];
+					selection.push(selectedCard);
+				} else {
+					throw new Error('The card could not be found.');
+				}
+
+				return selection;
+			}, []);
+
+			return channel({
+				announce: getResult({ selectedCards })
+			})
+				.then(() => selectedCards);
+		});
+};
+
 module.exports = {
 	all,
+	chooseCards,
 	draw,
 	fillDeck,
 	getCardCounts,
