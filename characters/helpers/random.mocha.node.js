0b662e01060056a6db06b907187accf345b68c6b
David Brainer-Banker <dbrainer-banker@salesforce.com>
2018-01-11 12:06:59 -0700

    Refactor character helpers

---
+++ characters/helpers/random.mocha.node.js
@@ -0,0 +1,36 @@
+const { expect, sinon } = require('../../shared/test-setup');
+
+const Beastmaster = require('../beastmaster');
+const pause = require('../../helpers/pause');
+
+const randomCharacter = require('./random');
+
+describe('./characters/index.js', () => {
+	let pauseStub;
+
+	before(() => {
+		pauseStub = sinon.stub(pause, 'setTimeout');
+	});
+
+	beforeEach(() => {
+		pauseStub.callsArg(0);
+	});
+
+	afterEach(() => {
+		pauseStub.reset();
+	});
+
+	after(() => {
+		pause.setTimeout.restore();
+	});
+
+	it('can get a random character', () => {
+		const character = randomCharacter();
+
+		expect(character).to.be.an.instanceof(Beastmaster);
+		expect(character.monsters.length).to.equal(1);
+		expect(character.monsters[0].cards.length).to.equal(character.monsters[0].cardSlots);
+		expect(character.givenName).to.be.a('string');
+		expect(character.monsters[0].givenName).to.be.a('string');
+	});
+});
