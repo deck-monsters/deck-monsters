87b7665c75f33059dbfce2129df2c1222749527a
David Brainer-Banker <dbrainer-banker@salesforce.com>
2018-01-10 19:44:05 -0700

    Move use back to the base item

--- items/base.js
+++ items/base.js
@@ -43,6 +43,33 @@ class BaseItem extends BaseClass {
 		return this.constructor.flavors || this.options.flavors;
 	}
 
+	get used () {
+		return this.options.used || 0;
+	}
+
+	set used (used) {
+		this.setOptions({
+			used
+		});
+	}
+
+	use (character, monster) {
+		this.emit('used', {
+			character,
+			monster
+		});
+
+		// Generally speaking once something has been used the player will drop it but we'll increment
+		// a counter instead of boolean just in case the item can be used more than once
+		this.used += 1;
+
+		if (this.action) {
+			return this.action(character, monster);
+		}
+
+		return this.used;
+	}
+
 	look (channel) {
 		return Promise
 			.resolve()
--- items/potions/base.js
+++ items/potions/base.js
@@ -12,33 +12,6 @@ class BasePotion extends BaseItem {
 	get potionType () {
 		return this.itemType;
 	}
-
-	get used () {
-		return this.options.used || 0;
-	}
-
-	set used (used) {
-		this.setOptions({
-			used
-		});
-	}
-
-	use (character, monster) {
-		this.emit('used', {
-			character,
-			monster
-		});
-
-		// Generally speaking once something has been used the player will drop it but we'll increment
-		// a counter instead of boolean just in case the item can be used more than once
-		this.used += 1;
-
-		if (this.effect) {
-			return this.effect(character, monster);
-		}
-
-		return this.used;
-	}
 }
 
 BasePotion.eventPrefix = 'potion';
