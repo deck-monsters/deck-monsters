a77e8cf63659f25ed898179fba135e8e89f2afe2
David Brainer-Banker <dbrainer-banker@salesforce.com>
2018-01-18 12:34:11 -0700

    Add more targeting strategies

--- helpers/targeting-strategies.js
+++ helpers/targeting-strategies.js
@@ -1,6 +1,8 @@
 const sample = require('lodash.sample');
 
 const TARGET_HIGHEST_HP_PLAYER = 'TARGET_HIGHEST_HP_PLAYER';
+const TARGET_HIGHEST_XP_PLAYER = 'TARGET_HIGHEST_XP_PLAYER';
+const TARGET_LOWEST_HP_PLAYER = 'TARGET_LOWEST_HP_PLAYER';
 const TARGET_MAX_HP_PLAYER = 'TARGET_MAX_HP_PLAYER';
 const TARGET_NEXT_PLAYER = 'TARGET_NEXT_PLAYER';
 const TARGET_RANDOM_PLAYER = 'TARGET_RANDOM_PLAYER';
@@ -14,8 +16,43 @@ function getTarget ({ playerContestant, contestants = [], strategy = TARGET_NEXT
 				// Skip the player
 				if (contestant === playerContestant) return potentialTarget;
 
+				// Fists of virtue
 				// If another monster has higher current hp, target them instead
 				if (contestant.monster.hp > potentialTarget.monster.hp) {
+					return contestant;
+				}
+
+				// Otherwise, continue
+				return potentialTarget;
+			}, defaultTarget);
+		}
+		case TARGET_HIGHEST_XP_PLAYER: {
+			const defaultTarget = getTarget({ playerContestant, contestants });
+
+			return contestants.reduce((potentialTarget, contestant) => {
+				// Skip the player
+				if (contestant === playerContestant) return potentialTarget;
+
+				// Maximize potential win bonus
+				// If another monster has higher current xp, target them instead
+				if (contestant.monster.xp > potentialTarget.monster.xp) {
+					return contestant;
+				}
+
+				// Otherwise, continue
+				return potentialTarget;
+			}, defaultTarget);
+		}
+		case TARGET_LOWEST_HP_PLAYER: {
+			const defaultTarget = getTarget({ playerContestant, contestants });
+
+			return contestants.reduce((potentialTarget, contestant) => {
+				// Skip the player
+				if (contestant === playerContestant) return potentialTarget;
+
+				// Fists of villainy
+				// If another monster has lower current hp, target them instead
+				if (contestant.monster.hp < potentialTarget.monster.hp) {
 					return contestant;
 				}
 
@@ -30,6 +67,7 @@ function getTarget ({ playerContestant, contestants = [], strategy = TARGET_NEXT
 				// Skip the player
 				if (contestant === playerContestant) return potentialTarget;
 
+				// Take down the big boss
 				// If another monster has higher maxHp, target them instead
 				if (contestant.monster.maxHp > potentialTarget.monster.maxHp) {
 					return contestant;
@@ -40,8 +78,10 @@ function getTarget ({ playerContestant, contestants = [], strategy = TARGET_NEXT
 			}, defaultTarget);
 		}
 		case TARGET_RANDOM_PLAYER: {
+			// Skip the player
 			const potentialTargets = contestants.filter(contestant => (contestant !== playerContestant));
 
+			// Pick any random target besides yourself
 			return sample(potentialTargets);
 		}
 		case TARGET_NEXT_PLAYER:
@@ -58,6 +98,8 @@ function getTarget ({ playerContestant, contestants = [], strategy = TARGET_NEXT
 
 module.exports = {
 	TARGET_HIGHEST_HP_PLAYER,
+	TARGET_HIGHEST_XP_PLAYER,
+	TARGET_LOWEST_HP_PLAYER,
 	TARGET_MAX_HP_PLAYER,
 	TARGET_NEXT_PLAYER,
 	TARGET_RANDOM_PLAYER,
