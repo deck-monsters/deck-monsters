d8b5cb8aa74485fe9b6a2064c3c32949467149ac
David Brainer-Banker <dbrainer-banker@salesforce.com>
2018-01-19 15:02:44 -0700

    Add the default targeting strategy card

---
+++ items/scrolls/clever-hans.js
@@ -0,0 +1,28 @@
+/* eslint-disable max-len */
+
+const TargetingScroll = require('./targeting');
+const { TARGET_NEXT_PLAYER } = require('../../helpers/targeting-strategies');
+
+// The "default" scroll
+class CleverHansScroll extends TargetingScroll {
+	// Set defaults for these values that can be overridden by the options passed in
+	constructor ({
+		icon = '🐎'
+	} = {}) {
+		super({ icon });
+	}
+
+	getTargetingDetails (monster) { // eslint-disable-line class-methods-use-this
+		return `${monster.givenName} will keep ${monster.pronouns.his} friends close and ${monster.pronouns.his} enemies closer, always attacking the next opponent in line unless directed otherwise by a specific card.`;
+	}
+}
+
+CleverHansScroll.itemType = 'The Gospel According to Clever Hans';
+CleverHansScroll.numberOfUses = 0;
+CleverHansScroll.description = `So it is said that if you know your enemy and know yourself, you will not be put at risk even in a hundred battles. If you only know yourself, but not your opponent, you may win or may lose. If you know neither yourself nor your enemy, you will always endanger yourself.
+
+Keep your strategy simple: your opponent is always the person next to you.`;
+CleverHansScroll.level = 0;
+CleverHansScroll.targetingStrategy = TARGET_NEXT_PLAYER;
+
+module.exports = CleverHansScroll;
---
+++ items/scrolls/clever-hans.mocha.node.js
@@ -0,0 +1,41 @@
+/* eslint-disable max-len */
+const { expect, sinon } = require('../../shared/test-setup');
+
+const pause = require('../../helpers/pause');
+const CleverHansScroll = require('./clever-hans');
+const Jinn = require('../../monsters/jinn');
+
+describe('./items/scrolls/clever-hans.js', () => {
+	let channelStub;
+	let pauseStub;
+
+	before(() => {
+		channelStub = sinon.stub();
+		pauseStub = sinon.stub(pause, 'setTimeout');
+	});
+
+	beforeEach(() => {
+		channelStub.resolves();
+		pauseStub.callsArg(0);
+	});
+
+	afterEach(() => {
+		channelStub.reset();
+		pauseStub.reset();
+	});
+
+	after(() => {
+		pause.setTimeout.restore();
+	});
+
+	it('can be instantiated with defaults', () => {
+		const cleverHans = new CleverHansScroll();
+		const jenn = new Jinn({ name: 'jenn', acVariance: 0, xp: 1300, gender: 'female' });
+
+		expect(cleverHans).to.be.an.instanceof(CleverHansScroll);
+		expect(cleverHans.numberOfUses).to.equal(0);
+		expect(cleverHans.expired).to.be.false;
+		expect(cleverHans.stats).to.equal('Usable an unlimited number of times.');
+		expect(cleverHans.getTargetingDetails(jenn)).to.equal('Jenn will keep her friends close and her enemies closer, always attacking the next opponent in line unless directed otherwise by a specific card.');
+	});
+});
