2f433331a8193c47958453f8d74401b967b336c8
David Brainer-Banker <dbrainer-banker@salesforce.com>
2018-01-19 12:32:18 -0700

    Fix the targeting strategy of La Carambada

--- items/scrolls/la-carambada.js
+++ items/scrolls/la-carambada.js
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
 
 const TargetingScroll = require('./targeting');
-const { TARGET_HIGHEST_HP_PLAYER } = require('../../helpers/targeting-strategies');
+const { TARGET_MAX_HP_PLAYER } = require('../../helpers/targeting-strategies');
 
 class LaCarambadaScroll extends TargetingScroll {
 	// Set defaults for these values that can be overridden by the options passed in
@@ -20,6 +20,6 @@ LaCarambadaScroll.itemType = 'The Ballad of La Carambada';
 LaCarambadaScroll.description = `Junto a ellos, aterrorizó la comarca, aguardando el día de la venganza. Hizo fama por su diestro manejo de la pistola, del machete y, sobre todo, por su extraordinaria habilidad para cabalgar. En tiempos en que las mujeres acompañaban a sus hombres a un lado del caballo, ver a una mujer galopando era un acontecimiento mayor.
 
 Target whoever has the highest maximum hp in the ring (other than yourself) even if they currently have less hp.`;
-LaCarambadaScroll.targetingStrategy = TARGET_HIGHEST_HP_PLAYER;
+LaCarambadaScroll.targetingStrategy = TARGET_MAX_HP_PLAYER;
 
 module.exports = LaCarambadaScroll;
