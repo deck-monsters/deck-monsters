38f9298eaa2d542d7868464b509fd982e2765269
Christopher McCulloh <cmcculloh@salesforce.com>
2018-01-24 12:37:16 -0500

    add very very basic unit tests

---
+++ helpers/sort.mocha.node.js
@@ -0,0 +1,45 @@
+const { expect } = require('../shared/test-setup');
+
+const pause = require('../helpers/pause');
+
+const { sort } = require('./sort');
+
+describe.only('./helpers/sort.js', () => {
+	let pauseStub;
+	let unsortedCards = [
+		{cardType: 'b', level: 1},
+		{cardType: 'a', level: 0},
+		{cardType: 'd', level: 2}
+	];
+	let sortedCards = [
+		{cardType: 'a', level: 0},
+		{cardType: 'b', level: 1},
+		{cardType: 'd', level: 2}
+	];;
+
+	before(() => {
+		pauseStub = sinon.stub(pause, 'setTimeout');
+	});
+
+	beforeEach(() => {
+		pauseStub.callsArg(0);
+	});
+
+	afterEach(() => {
+		pauseStub.reset();
+	});
+
+	after(() => {
+		pause.setTimeout.restore();
+	});
+
+	describe('sort', () => {
+		it('sorts by cardType by default', () => {
+			expect(sort(unsortedCards)).to.deep.equal(sortedCards);
+		});
+
+		it('sorts by level if specified', () => {
+			expect(sort(unsortedCards, 'level')).to.deep.equal(sortedCards);
+		})
+	});
+});
