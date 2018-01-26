314d71fb7b1b2b40d654c3e0b8df98114d69e450
David Brainer-Banker <dbrainer-banker@salesforce.com>
2018-01-26 09:51:21 -0700

    Add item announcements

---
+++ announcements/index.js
@@ -0,0 +1,64 @@
+const announceBossWillSpawn = require('./bossWillSpawn.js');
+const announceCardDrop = require('./cardDrop.js');
+const announceCardPlayed = require('./card-played.js');
+const announceContestant = require('./contestant.js');
+const announceContestantLeave = require('./contestantLeave.js');
+const announceDeath = require('./death.js');
+const announceEffect = require('./effect.js');
+const announceEndOfDeck = require('./endOfDeck.js');
+const announceFight = require('./fight.js');
+const announceFightConcludes = require('./fightConcludes.js');
+const announceHeal = require('./heal.js');
+const announceHit = require('./hit.js');
+const announceItemUsed = require('./item-used.js');
+const announceLeave = require('./leave.js');
+const announceMiss = require('./miss.js');
+const announceModifier = require('./modifier.js');
+const announceNarration = require('./narration.js');
+const announceNextRound = require('./nextRound.js');
+const announceRolled = require('./rolled.js');
+const announceStay = require('./stay.js');
+const announceTurnBegin = require('./turnBegin.js');
+const announceXPGain = require('./xpGain.js');
+
+const initialize = (game) => {
+	const events = [
+		{ event: 'card.effect', listener: announceEffect },
+		{ event: 'card.miss', listener: announceMiss },
+		{ event: 'card.narration', listener: announceNarration },
+		{ event: 'card.played', listener: announceCardPlayed },
+		{ event: 'card.rolled', listener: announceRolled },
+		{ event: 'card.stay', listener: announceStay },
+		{ event: 'cardDrop', listener: announceCardDrop },
+		{ event: 'creature.die', listener: announceDeath },
+		{ event: 'creature.hit', listener: announceHit },
+		{ event: 'creature.leave', listener: announceLeave },
+		{ event: 'creature.modifier', listener: announceModifier },
+		{ event: 'gainedXP', listener: announceXPGain },
+		{ event: 'item.narration', listener: announceNarration },
+		{ event: 'item.used', listener: announceItemUsed },
+		{ event: 'ring.add', listener: announceContestant },
+		{ event: 'ring.bossWillSpawn', listener: announceBossWillSpawn },
+		{ event: 'ring.endOfDeck', listener: announceEndOfDeck },
+		{ event: 'ring.fight', listener: announceFight },
+		{ event: 'ring.fightConcludes', listener: announceFightConcludes },
+		{ event: 'ring.gainedXP', listener: announceXPGain },
+		{ event: 'ring.narration', listener: announceNarration },
+		{ event: 'ring.remove', listener: announceContestantLeave },
+		{ event: 'ring.roundComplete', listener: announceNextRound },
+		{ event: 'ring.turnBegin', listener: announceTurnBegin }
+	];
+
+	events.map(event => game.on(event.event, (...args) => {
+		event.listener(game.publicChannel, game.channelManager, ...args);
+	}));
+
+	// this one needs ring, the others don't (or have it already).
+	game.on('creature.heal', (...args) => {
+		announceHeal(game.publicChannel, game.channelManager, game.ring, ...args);
+	});
+};
+
+module.exports = {
+	initialize
+};
