4baf5a86372e80a8888a0c7fd1e9676edc290897
Christopher McCulloh <cmcculloh@salesforce.com>
2018-01-28 12:08:16 -0500

    stat constants in alphabetical order

--- helpers/stat-constants.js
+++ helpers/stat-constants.js
@@ -1,24 +1,24 @@
-const MAX_PROP_MODIFICATIONS = {
-	ac: 1,
-	str: 1,
-	dex: 1,
-	int: 1,
-	xp: 40,
-	hp: 12
-};
-const BASE_AC = 5;
-const BASE_STR = 5;
-const BASE_DEX = 5;
-const BASE_INT = 5;
 const AC_VARIANCE = 2;
+const BASE_AC = 5;
+const BASE_DEX = 5;
 const BASE_HP = 28;
+const BASE_INT = 5;
+const BASE_STR = 5;
 const HP_VARIANCE = 5;
 const MAX_BOOSTS = {
+	ac: (BASE_AC * 2) + AC_VARIANCE,
 	dex: 10,
-	str: 6,
-	int: 8,
 	hp: (BASE_HP * 2) + HP_VARIANCE,
-	ac: (BASE_AC * 2) + AC_VARIANCE
+	int: 8,
+	str: 6
+};
+const MAX_PROP_MODIFICATIONS = {
+	ac: 1,
+	dex: 1,
+	hp: 12,
+	int: 1,
+	str: 1,
+	xp: 40
 };
 
 module.exports = {
