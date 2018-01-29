84f5c8f8c1016e0ac4ff6950fd68fb2afc672f49
David Brainer-Banker <dbrainer-banker@salesforce.com>
2018-01-28 17:26:46 -0700

    Add a couple of healing potions

---
+++ items/potions/healing.js
@@ -0,0 +1,46 @@
+/* eslint-disable max-len */
+
+const BaseScroll = require('./base');
+
+const { COMMON } = require('../../helpers/probabilities');
+const { REASONABLE } = require('../../helpers/costs');
+
+class HealingPotion extends BaseScroll {
+	constructor ({
+		icon = '💊'
+	} = {}) {
+		super({ icon });
+	}
+
+	get healAmount () {
+		return this.constructor.healAmount;
+	}
+
+	healingMessage (monster) {
+		return `${monster.givenName} drinks ${monster.pronouns.his} ${this.icon} ${this.itemType} for ${this.healAmount} hp.`;
+	}
+
+	action ({ channel, channelName, monster }) {
+		const { healAmount } = this;
+
+		if (monster && !monster.dead && healAmount) {
+			this.emit('narration', {
+				channel,
+				channelName,
+				narration: this.healingMessage(monster)
+			});
+
+			monster.heal(healAmount);
+		}
+	}
+}
+
+HealingPotion.itemType = 'Potion of Healing';
+HealingPotion.probability = COMMON.probability;
+HealingPotion.healAmount = 8;
+HealingPotion.numberOfUses = 1;
+HealingPotion.description = `Instantly heal ${HealingPotion.healAmount} hp.`;
+HealingPotion.level = 1;
+HealingPotion.cost = REASONABLE.cost;
+
+module.exports = HealingPotion;
--- items/potions/helpers/all.js
+++ items/potions/helpers/all.js
@@ -1,1 +1,7 @@
-module.exports = [];
+const ChocolateBar = require('../chocolate-bar');
+const HealingPotion = require('../healing');
+
+module.exports = [
+	ChocolateBar,
+	HealingPotion
+];
